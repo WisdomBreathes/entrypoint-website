b2933ba7b266dd81b4d244f75a6f71801feb18c4
Shehzeb Iftakhar <ishehzeb@gmail.com>
2026-04-10 01:38:33 -0400

    Add trailing slash to Astro base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://wisdombreathes.github.io',
-  base: '/entrypoint-website',
+  base: '/entrypoint-website/',
   integrations: [tailwind()],
 });
