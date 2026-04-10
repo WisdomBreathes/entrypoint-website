135f70729e85413715f4d447523e07f419dfbc75
Shehzeb Iftakhar <ishehzeb@gmail.com>
2026-04-10 03:47:24 -0400

    Switch Astro config to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://wisdombreathes.github.io',
-  base: '/entrypoint-website/',
+  site: 'https://entrypointcanada.com',
+  base: '/',
   integrations: [tailwind()],
 });
